8dfecd30e061d1ed85107b9d427a5ff9cf8d385d
Wallace Figueiredo <wallace.figueiredo@dataprev.gov.br>
2018-05-09 15:19:53 -0300

    Alteração do main

--- app.js
+++ app.js
@@ -4,10 +4,12 @@
 
 const CoffeeMachine = require('./coffee-machine');
 
-const coffeeMachine = new CoffeeMachine([0.05, 0.08, 0.10, 6.01, 6.70, 6.11, 6.03, 0.04, 0.08, 0.03, 0.07]);
+//const coffeeMachine = new CoffeeMachine([0.05, 0.08, 0.10, 6.01, 6.70, 6.11, 6.03, 0.04, 0.08, 0.03, 0.07]);
 
-coffeeMachine.on(CoffeeMachine.STAND_BY, () => console.log('STANDBY'));
-coffeeMachine.on(CoffeeMachine.WORKING, () => console.log('WORKING'));
-coffeeMachine.on(CoffeeMachine.FINISH, () => console.log('FINISH'));
+const coffeeMachine = new CoffeeMachine('/dev/ttyUSB0');
+
+coffeeMachine.on(CoffeeMachine.STAND_BY, () => console.log(`STANDBY ${new Date()}`));
+coffeeMachine.on(CoffeeMachine.WORKING, () => console.log(`WORKING ${new Date()}`));
+coffeeMachine.on(CoffeeMachine.FINISH, () => console.log(`FINISH ${new Date()}`));
 
 coffeeMachine.powerOn();
--- coffee-machine.js
+++ coffee-machine.js
@@ -1,42 +1,60 @@
 'use strict';
 
 const EventEmitter = require('events');
+const SerialPort = require('serialport');
+const ReadLine = SerialPort.parsers.Readline;
 
 class CoffeeMachine extends EventEmitter {
-    constructor(array) {
+    constructor(address) {
         super();
 
-        if (!array || array.length <= 0) throw new Error('Array not defined!!');
+        //if (!array || array.length <= 0) throw new Error('Array not defined!!');
 
-        this._array = array;
+        //this._array = array;
+        this._port = new SerialPort(address || process.env.SERIAL_PORT_ADDRESS, {
+            baudRate: 9600,
+            dataBits: 8, 
+            parity: 'none', 
+            stopBits: 1, 
+            flowControl: false 
+        });
+
+        this._port.pipe(new ReadLine());
+
         this._status = { 
             current: CoffeeMachine.POWER_OFF,
             previous: null,
         }
 
-        this.on(CoffeeMachine.STAND_BY, () => this._status.current = CoffeeMachine.STAND_BY);
+        /*this.on(CoffeeMachine.STAND_BY, () => this._status.current = CoffeeMachine.STAND_BY);
         this.on(CoffeeMachine.WORKING, () => this._status.current = CoffeeMachine.WORKING);
         this.on(CoffeeMachine.FINISH, () => this._status.current = CoffeeMachine.FINISH);
+        */
     }
 
     powerOn() {
-        const sleep = ms => new Promise(res => setTimeout(res, ms));
-
-        const read = (num, cb) => sleep(1500).then(() => {
-            cb && cb();
-        });
-
-        this._array.reduce((promise, element) => {
-            return promise.then(() => read(element, () => {
+        const rgx = new RegExp(/\d.\d\d/);
+        this._port.on('open', () => {
+            console.log('\n Coffee Machine connected! \n');
+            this._port.on('data', data => {
+                let irms = Number.parseFloat(rgx.exec(data.toString())[0]);
+                //console.log(irms);
                 this._status.previous = this._status.current;
-
-                if (element < 6.00 && this._status.previous === CoffeeMachine.WORKING)
-                    this.emit(CoffeeMachine.FINISH);
-                else if (element >= 6.00)
-                    this.emit(CoffeeMachine.WORKING);
-                else this.emit(CoffeeMachine.STAND_BY); 
-            }));
-        }, Promise.resolve());
+                if (irms < 6.00 && this._status.previous === CoffeeMachine.WORKING) {
+                    this._status.current = CoffeeMachine.FINISH;
+                    if (this._status.previous !== CoffeeMachine.FINISH)
+                        this.emit(CoffeeMachine.FINISH);
+                } else if (irms >= 6.00) {
+                    this._status.current = CoffeeMachine.WORKING
+                    if (this._status.previous !== CoffeeMachine.WORKING)
+                        this.emit(CoffeeMachine.WORKING);
+                } else {
+                    this._status.current = CoffeeMachine.STAND_BY;
+                    if (this._status.previous != CoffeeMachine.STAND_BY)
+                        this.emit(CoffeeMachine.STAND_BY);
+                }
+            });  
+        });
     }
 
     static get POWER_OFF() {
